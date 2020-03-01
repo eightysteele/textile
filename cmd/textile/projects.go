b823ef1ac8c35c51775269685e25e883c2325f05
Aaron Steele <eightysteele@gmail.com>
2020-03-01 15:18:22 -0800

    initial support for `textile projects shell` which does a os/exec to thread-shell

--- cmd/textile/projects.go
+++ cmd/textile/projects.go
@@ -18,7 +18,7 @@ import (
 func init() {
 	rootCmd.AddCommand(projectsCmd)
 	rootCmd.AddCommand(initProjectCmd) // alias to root
-	projectsCmd.AddCommand(initProjectCmd, lsProjectsCmd, inspectProjectCmd, rmProjectCmd)
+	projectsCmd.AddCommand(initProjectCmd, lsProjectsCmd, inspectProjectCmd, rmProjectCmd, shellProjectCmd)
 
 	initProjectCmd.Flags().String(
 		"path",
@@ -133,6 +133,83 @@ var inspectProjectCmd = &cobra.Command{
 			[]string{"name", "id", "store", "address", "balance"},
 			[][]string{{selected.Name, selected.ID, selected.StoreID, selected.WalletAddress,
 				strconv.FormatInt(selected.WalletBalance, 10)}})
+	},
+}
+
+var shellProjectCmd = &cobra.Command{
+	Use:   "shell",
+	Short: "Shell into a project",
+	Long:  `Shel into the ThreadDB of a project (interactive).`,
+	Run: func(c *cobra.Command, args []string) {
+		selected := selectProject("Select project", aurora.Sprintf(
+			aurora.BrightBlack("> Selected {{ .Name | white | bold }}")))
+		cmd.Message(authViper.GetString("token"))
+		cmd.Message(selected.StoreID)
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+
+		os.Chdir("/Users/eighty/github/eightysteele/thread-shell")
+		mydir, err := os.Getwd()
+		if err == nil {
+			fmt.Println(mydir)
+		}
+		// Set an environment variable.
+		os.Setenv("SOME_VAR", "1")
+
+		// get project token
+		ctx, cancel := context.WithTimeout(context.Background(), cmdTimeout)
+		defer cancel()
+		tokens, err := client.ListTokens(
+			ctx,
+			selected.Name,
+			api.Auth{
+				Token: authViper.GetString("token"),
+			})
+		if err != nil {
+			cmd.Fatal(err)
+		}
+		// if len(tokens.List) > 0 {
+		// data := make([][]string, len(tokens.List))
+		// for i, t := range tokens.List {
+		// 	data[i] = []string{t}
+		// }
+		// }
+
+		// Transfer stdin, stdout, and stderr to the new process
+		// and also set target directory for the shell to start in.
+		pa := os.ProcAttr{
+			Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+			Dir:   mydir,
+		}
+
+		// Start up a new shell.
+		// Note that we supply "login" twice.
+		// -fpl means "don't prompt for PW and pass through environment."
+		fmt.Print(">> Starting a new shell")
+		proc, err := os.StartProcess(
+			"shell",
+			[]string{
+				"shell",
+				"--project_token", tokens.GetList()[0], // @todo: hack, how handle multiple tokens?
+				"--project_db_id", selected.StoreID,
+				"--project_db_name", selected.Name},
+			&pa)
+		if err != nil {
+			panic(err)
+		}
+
+		// Wait until user exits the shell
+		state, err := proc.Wait()
+		if err != nil {
+			panic(err)
+		}
+
+		// Chang
+		os.Chdir(cwd)
+		fmt.Printf("<< Exited shell: %s\n", state.String())
 	},
 }
 
